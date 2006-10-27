29418ae6fe2033f9768bd01337b0445ddb56a646
Nicolas Grekas <nicolas.grekas@gmail.com>
2006-10-27 13:38:36 +0000

    ++ licensed code under GPLv2+

--- class/jsquiz.php
+++ class/jsquiz.php
@@ -1,4 +1,16 @@
-<?php
+<?php /*********************************************************************
+ *
+ *   Copyright : (C) 2006 Nicolas Grekas. All rights reserved.
+ *   Email     : nicolas.grekas+patchwork@espci.org
+ *   License   : http://www.gnu.org/licenses/gpl.txt GNU/GPL, see COPYING
+ *
+ *   This program is free software; you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation; either version 2 of the License, or
+ *   (at your option) any later version.
+ *
+ ***************************************************************************/
+
 
 /*
 * This class obfuscates javascript code
