d136bafc00b8fb586c1821e381b0145c184e54c7
JakeQZ <jake@qzdesign.co.uk>
2017-04-10 20:14:28 +0100

    Fix `;` incorrectly inserted after for/if/while and other keywords followed by prefix operator
    The previous change adds semicolons that shouldn't be in constructs like
    `while(x)\n--x`, `else\n--x`, `do\n--x` and `return --\nx`.
    
    This is addressed for the `for`/`if`/`while` case by marking a potential
    semicolon after a closing bracket with a `#` symbol, and handling it
    later when removing semicolons in the loop where the state of preceding
    `for`/`if`/`while` expressions is maintained.  To cater for `do..while`
    statements, additional state is now maintained so as to correctly
    determine whether a `while` corresponds to a `do`.  Additionally, this
    fixes a pre-existing bug where a semicolon was not always added after a
    `do..while`.
    
    For other keywords, their presence can simply be checked with a regex so
    that e.g. `else++` is not treated as postincrement and `else\n++x` not
    as separate statements.
    
    Also spotted that the `substr` length wasn't enough for excluding
    `continue` at the end of a variable name when adding a semicolon after,
    and corrected this.

--- tests/expected/postfix-prefix.js
+++ tests/expected/postfix-prefix.js
@@ -1,9 +1,19 @@
-;function g(r){var f;r(f--);for(var n=0;n<parseInt('10 )');n++){++r};for(var a=0;a--;a++){r++};for(var a=1;--a;++a){++r}(r++)?(r++):(r++);while(r++){r--};for(r=(function(){return 10})();r--;r++){r--&&f++,f++;f=f++ +2};for(r=(function(){return 10})();--r;++r){--r&&++f,++f;f=2+ ++f};if(r++){--r}
-else if(r++){--r};if(++r){r--}
-else if(++r){r--};return r};g(function(r){r++;r--;++r;++r;++r;while(1){break;continue;return};var t={w:r++};var e={w:r++};var i={w:++r};var v={w:++r};var n={w:++r};var f=[r++],a=[++r]});var w;++w;var x;x=2;x++;x++;(y=2);x&&++(y);++(y);var v;v++;var z;z=2;++z;try{++z;++z(y=2);z&&(y)++}catch(r$){};z&&(y)++;(y)++;x++;{x++};++x;{++x};f(f(w++));f(f(++w));f(f(++w));function h(r){var f;r(f--);for(var n=0;n<parseInt('10 )');n++){++r};for(var a=0;a--;a++){r++};for(var a=1;--a;++a){++r}(r++)?(r++):(r++);while(r++){r--};for(r=(function(){return 10})();r--;r++){r--&&f++,f++;f=f++ +2};for(r=(function(){return 10})();--r;++r){--r&&++f,++f;f=2+ ++f};if(r++){--r}
-else if(r++){--r};if(++r){r--}
-else if(++r){r--};return r};g(function(r){r++;r--;++r;++r;var v={w:r++};var n={w:r++};var a={w:++r};var f=[r++]});var w;++w;var x;x=2;x++;x++;(y=2);var v;v++;var z;z=2;++z;try{++z;++z(y=2);z&&(y)++}catch(r$){};z&&(y)++;(y)++;x++;{x++};++x;{++x};f(f(w++));f(f(++w));
+;function g(e){var i;e(i--);for(var w=0;w<parseInt('10 )');w++){++e};for(var r=0;r--;r++){e++};for(var r=1;--r;++r){++e}(e++)?(e++):(e++);while(e++){e--};for(e=(function(){return 10})();e--;e++){e--&&i++,i++;i=i++ +2};for(e=(function(){return 10})();--e;++e){--e&&++i,++i;i=2+ ++i};if(e++){--e}
+else if(e++){--e};if(++e){e--}
+else if(++e){e--};return e};g(function(e){e++;e--;++e;++e;++e;while(1){break;continue;xcontinue=4;return};var o={w:e++};var n={w:e++};var l={w:++e};var f={w:++e};var w={w:++e};var i=[e++],r=[++e]});var w;++w;var x;x=2;x++;x++;(y=2);x&&++(y);++(y);var v;v++;var z;z=2;++z;try{++z;++z(y=2);z&&(y)++}catch(e$){};z&&(y)++;(y)++;x++;{x++};++x;{++x};f(f(w++));f(f(++w));f(f(++w));function h(e){var i;e(i--);for(var w=0;w<parseInt('10 )');w++){++e};for(var r=0;r--;r++){e++};for(var r=1;--r;++r){++e}(e++)?(e++):(e++);while(e++){e--};for(e=(function(){return 10})();e--;e++){e--&&i++,i++;i=i++ +2};for(e=(function(){return 10})();--e;++e){--e&&++i,++i;i=2+ ++i};if(e++){--e}
+else if(e++){--e};if(++e){e--}
+else if(++e){e--};return e};g(function(e){e++;e--;++e;++e;var f={w:e++};var w={w:e++};var r={w:++e};var i=[e++]});var w;++w;var x;x=2;x++;x++;(y=2);var v;v++;var z;z=2;++z;try{++z;++z(y=2);z&&(y)++}catch(e$){};z&&(y)++;(y)++;x++;{x++};++x;{++x};f(f(w++));f(f(++w));
 /*! Issue #47 */
-;function issue47(r,f,a){var n,v;if(!r){for(r=[],n=f.childNodes||f;(v=n[a])!=null;a++){}}};
+;function issue47(e,i,r){var w,f;if(!e){for(e=[],w=i.childNodes||i;(f=w[r])!=null;r++){}}};
 /*! Issue #49 */
-;function issue49(r,f,a){r.push(f);++a};
+;function issue49(e,i,r){e.push(i);++r};
+/*! Pull #50 comment */
+;function pull50(e){while(e)--e;while(e)e--;while(e)--e;while(e)--e;while(e)--e;while(e)e--;while(e)--e;while(e)e--;while(e)--e;while(e)--e;while(e)--e;while(e)e--;while(e)--e;while(e)e--;while(e)--e;while(e)e--};
+/*! do-while */
+;function doWhile(e){do{e--}
+while(e--);e--;do e--;while(e--);e--;do{--e}
+while(--e);--e;do--e;while(--e);--e;do while(e--)e--;while(e--);e--;do{while(e--)e--;do e--;while(e--);e--}
+while(e--);e--;do while(--e)--e;while(--e);--e;do{while(--e)--e;do--e;while(--e);--e}
+while(--e);--e};
+/*! other keywords */
+;function keywords(e){if(e)--e;else++e;if(e)--e;else++e;if(typeof++e==='string'||typeof e++==='string')throw++e;throw;++e;return++e;return;++e};
